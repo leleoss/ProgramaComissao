d5e19ac49323de228806ef40bc4211baf375ba0b
user_acronus <user_acronus@senacsp.edu.br>
2018-04-23 13:33:26 -0300

    Programa de Comissão

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace ProgramaComissao
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+             double valorMeta, perComissao, valorVenda, valorComissao;
+
+            Console.Clear();//Limpa a tela de console 
+            Console.WriteLine("Digite o valor da meta");
+            valorMeta = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("Digite o percentual de comissão");
+            perComissao = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("Digite o valor da venda");
+            valorVenda = double.Parse(Console.ReadLine());
+
+            if(valorVenda>=valorMeta)
+                valorComissao = valorVenda*(perComissao/100);
+            else
+                valorComissao = 0;
+
+            Console.WriteLine("O valor da comissão é:"+valorComissao);
+        }
+    }
+}
